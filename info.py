ef2843c5d65b3581382bc2a7642ea0e1a9d985f7
⌯ Ꭺɴᴏɴʏᴍᴏᴜꜱ | ×͜× | <155171427+NBBotz@users.noreply.github.com>
2024-10-01 09:45:45 +0530

    Fixed

--- info.py
+++ info.py
@@ -133,7 +133,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL")
+URL = environ.get("URL", "silextxbotz-stream-blacktiger-7c583708.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
