5cac615c0da4808a1e4d9839e487da90b6c02649
Genz Alok <genzalok24@gmail.com>
2025-03-09 16:29:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -93,11 +93,11 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 # Token Verification Info :
 VERIFY = bool(environ.get('VERIFY', True))
 VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
-VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'seturl.in')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'ddc2099afd2fd802a464d3894fd489b3d696184b')
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'techvjlink.site')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '44dcf9d1f23870de34d549f099828b358b21ce45')
 # if verify second shortner is True then fill below url and api
-VERIFY_SND_SHORTLINK_URL = environ.get('VERIFY_SND_SHORTLINK_URL', 'seturl.in')
-VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', 'ddc2099afd2fd802a464d3894fd489b3d696184b')
+VERIFY_SND_SHORTLINK_URL = environ.get('VERIFY_SND_SHORTLINK_URL', 'techvjlink.site')
+VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '44dcf9d1f23870de34d549f099828b358b21ce45')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/FRverfication0/3')
 
 # Shortlink Info
