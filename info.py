edb534568b30c86811443230fe98d2093e534bc5
Genz Alok <genzalok24@gmail.com>
2025-04-21 10:15:52 +0530

    Shortner Off info.py

--- info.py
+++ info.py
@@ -91,7 +91,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', True))
+VERIFY = bool(environ.get('VERIFY', False))
 VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'urlxpro.com')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '17d2876b07a59d2d4c98447243be1efcc45143c3')
